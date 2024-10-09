842948e2706cba789b440ecddadd3f7f65cae51d
JennyBooW <verikgorohova@yandex.com>
2024-10-09 13:24:46 +0300

    Finished exercieses!!! :)

--- exercises.lean
+++ exercises.lean
@@ -1,25 +1,53 @@
 -- Exercises
--- Another Representation
--- An alternative way to represent a positive number is as the successor of some Nat.
--- Replace the definition of Pos with a structure whose constructor is named succ
--- that contains a Nat:
--- structure Pos where
---   succ ::
---   pred : Nat
--- Define instances of Add, Mul, ToString, and OfNat that allow this version of
--- Pos to be used conveniently.
-namespace Positive
+-- Define Type that allows only Even numbers to be represented by it.
 
-end Positive
-
-
-
--- Even Numbers
--- Define a datatype that represents only even numbers. Define instances of Add, Mul, and ToString that allow it to be used conveniently. OfNat requires a feature that is introduced in the next section.
 namespace Even
 
-end Even
+inductive Even : Type where
+  | zero : Even
+  | succ : Even -> Even
 
+def Even.plus : Even → Even → Even
+  | Even.zero, k   => Even.succ k
+  | Even.succ n, k => Even.succ (n.plus k)
+
+instance : Add Even where
+  add := Even.plus
+
+def Even.mul : Even → Even → Even
+  | Even.zero, _   => Even.zero
+  | Even.succ n, k => n.mul k + k
+
+instance : Mul Even where
+  mul := Even.mul
+
+instance : OfNat Even n where
+  ofNat :=
+    let rec natToEven : Nat → Even
+    | 0     => Even.zero
+    | n + 1 =>
+    if n % 2 == 0
+    then Even.succ (natToEven n)
+    else natToEven n
+  natToEven n
+
+def Even.toNat : Even → Nat
+  | Even.zero   => 0
+  | Even.succ n => n.toNat + 2
+
+instance : ToString Even where
+  toString x := toString (x.toNat)
+
+
+def two  : Even := 2
+def four : Even := 4
+def five : Even := 5
+
+theorem isFour    : two * two = four := rfl
+theorem isEigth   : two * two * two = four * two := rfl
+theorem isSixteen : (two * two) * (two * two) = four * four := rfl
+
+end Even
 
 -- HTTP Requests
 -- An HTTP request begins with an identification of a HTTP method,
@@ -30,4 +58,42 @@ end Even
 -- associate different IO actions with each HTTP method, and write a test harness as
 -- an IO action that calls each method and prints the result.
 namespace Requests
+
+inductive Requests : Type where
+  | GET    : Requests
+  | POST   : Requests
+  | PUT    : Requests
+  | DELETE : Requests
+
+structure HttpResponse where
+  statusCode : Nat
+  body       : String
+deriving Inhabited, Repr
+
+instance : ToString HttpResponse where
+  toString (response : HttpResponse) : String :=
+  s!"Status Code: {response.statusCode}\nBody: {response.body}"
+
+class HttpAction (m : Type → Type) where
+  execute : Requests → m HttpResponse
+
+instance : HttpAction IO where
+  execute method :=
+    match method with
+    | Requests.GET      => pure { statusCode := 200, body := "GET request successuful"   : HttpResponse}
+    | Requests.POST     => pure { statusCode := 201, body := "POST request successuful"  : HttpResponse}
+    | Requests.PUT      => pure { statusCode := 204, body := "PUT request successuful"   : HttpResponse}
+    | Requests.DELETE   => pure { statusCode := 200, body := "DELTE request successuful" : HttpResponse }
+
+
+def testHttp : IO Unit := do
+  for method in #[Requests.GET, Requests.POST, Requests.PUT, Requests.DELETE] do
+    let response ← HttpAction.execute method
+    IO.println response
+
+def main : IO Unit :=
+  testHttp
+
+#eval main
+
 end Requests
